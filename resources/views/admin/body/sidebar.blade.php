ef0babfc12bd6b5e36ea60a862b9b3922dc2017d
mdrakash <75482117+mdrakash@users.noreply.github.com>
2022-09-06 15:38:35 +0600

    update

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -15,7 +15,7 @@
 				  <!-- logo for regular state and mobile devices -->
 					 <div class="d-flex align-items-center justify-content-center">					 	
 						  <img src="{{asset('backend/images/logo-dark.png')}}" alt="">
-						  <h3><b>Easy</b> Admin</h3>
+						  <h3><b>Fresh</b> Admin</h3>
 					 </div>
 				</a>
 			</div>
